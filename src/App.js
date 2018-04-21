2249f861cb8b4a1710c11afe745f05767af2d973
Landon <landonchristopherrichards@gmail.com>
2018-04-21 05:37:19 -0700

    final commit?

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import Create from "./components/Create.js";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
 import './App.css';
 
 class App extends Component {
@@ -9,7 +14,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      upvotes: 0,
+      downvotes: 0
     };
   }
 
@@ -32,6 +39,13 @@ class App extends Component {
     window.location.reload();
   }
 
+  // const myData = [upvotes, downvotes].concat(this.state.data)
+  //   .sort((a, b) => a.itemupM > b.itemM)
+  //   .map((item, i) => 
+  //       <div key={i}> {item.matchID} {item.timeM}{item.description}</div>
+  //   );
+
+
   render() {
     return (
       <div class="container">
@@ -42,23 +56,42 @@ class App extends Component {
               {localStorage.getItem('jwtToken') &&
                 <button class="btn btn-primary" onClick={this.logout}>Logout</button>
               }
+             <Link to="/create">
+     <button class="btn btn-primary" type="button">
+          Create a New post!
+     </button>
+ </Link>
             </h3>
           </div>
           <div class="panel-body">
             <table class="table table-stripe">
               <thead>
                 <tr>
+                  <th>Guide Raiting</th>
                   <th>Game Name</th>
                   <th>Title</th>
+                  <th>Difficulty</th>
                   <th>Author</th>
                 </tr>
               </thead>
               <tbody>
                 {this.state.books.map(book =>
                   <tr>
-                    <td><Link to={`/show/${book._id}`}>{book.isbn}</Link></td>
-                    <td>{book.title}</td>
-                    <td>{book.author}</td>
+        
+                  <td>
+
+        <a className="upvote" onClick={() => {
+            this.setState({ups: this.state.upvotes + 1})
+          }}><img src={"images/wednesday.png"} id="upvote" /> {this.state.ups}</a>
+        
+        <a className="downvote" onClick={() => {
+            this.setState({downs: this.state.downvotes + 1})
+          }}><img src={"images/tuesday.png"} id="downvote"/> {this.state.downs}</a>
+          </td>
+                    <td><Link to={`/show/${book._id}`}>{book.origin}</Link></td>
+                    <td>{book.name}</td>
+                    <td>{book.difficulty}</td>
+                    <td>{book.guide}</td>
                   </tr>
                 )}
               </tbody>
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from "../images/tonberryLandscapeBlack.png";
+
+class Header extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+    
+  }
+    render () {
+    return (
+    	<div id="header">
+      		<img src={logo} id="TonberryLogo" alt="Logo"/>
+      	</div>
+    );
+  }
+}
+
+export default Header;
